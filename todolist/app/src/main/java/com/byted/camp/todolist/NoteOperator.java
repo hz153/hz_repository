f9a62b057e186b8b51b40f051b418c3373104042
hz153 <101552805@qq.com>
2021-04-01 16:18:57 +0800

    安卓开发实训第六次作业

---
+++ todolist/app/src/main/java/com/byted/camp/todolist/NoteOperator.java
@@ -0,0 +1,13 @@
+package com.byted.camp.todolist;
+
+
+import com.byted.camp.todolist.beans.Note;
+
+public interface NoteOperator {
+
+    void deleteNote(Note note);
+
+    void updateNote(Note note);
+
+    void changeNote(Note note);
+}
